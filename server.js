bbe927aae61ad1f4745979a7180b541553e3126e
TimoW75 <timo2003werkhoven@gmail.com>
2022-02-15 13:15:57 +0100

    Server.js update

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+ express()
+    // .use('/static',express.static(‘static’))
+    .get('/', onhome)
+    .get('/profiel', onprofiel)
+    .get('/login', onlogin)
+    .listen(8000)
+    
+function onhome(req, res) {
+    res.send('<h1>Hello World!</h1>')
+}
+
+function onprofiel(req, res) {
+    res.send('<h1>Profiel</h1>')
+}
+
+function onlogin(req, res) {
+    res.send('<h1>Inloggen</h1>')
+}
