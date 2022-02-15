8b95fd9c809a3187809f93c3dc4e5bc7937912f3
TimoW75 <timo2003werkhoven@gmail.com>
2022-02-15 13:31:30 +0100

    ES5 to ES6

--- server.js
+++ server.js
@@ -2,19 +2,12 @@ const express = require('express')
 
  express()
     // .use('/static',express.static(‘static’))
-    .get('/', onhome)
-    .get('/profiel', onprofiel)
-    .get('/login', onlogin)
+
+    .get('/', (req, res) => { res.send('<h1>Hello World!</h1>')})
+
+    .get('/profiel', (req, res) => { res.send('<h1>Profiel</h1>')})
+
+    .get('/library', (req, res) => { res.send('<h1>Library</h1>')})
+    
     .listen(3000)
     
-function onhome(req, res) {
-    res.send('<h1>Hello World!</h1>')
-}
-
-function onprofiel(req, res) {
-    res.send('<h1>Profiel</h1>')
-}
-
-function onlogin(req, res) {
-    res.send('<h1>Inloggen</h1>')
-}
