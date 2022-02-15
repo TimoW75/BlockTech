0fa47d751194afe34386140e1a9a34733c1d12eb
TimoW75 <timo2003werkhoven@gmail.com>
2022-02-15 14:01:19 +0100

    static css

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express')
 
+
  express()
-    // .use('/static',express.static(‘static’))
+    .use('/static',express.static(‘static’))
 
-    .get('/', (req, res) => { res.send('<h1>Hello World!</h1>')})
+    .get('/', (req, res) => { 
+        res.send('<h1>Hello World!</h1>')
+        res.status(200)
+    })
 
-    .get('/profiel', (req, res) => { res.send('<h1>Profiel</h1>')})
+    .get('/profiel', (req, res) => { 
+        res.send('<h1>Profiel</h1>')
+        res.status(200)
+    })
 
-    .get('/library', (req, res) => { res.send('<h1>Library</h1>')})
+    .get('/library', (req, res) => { 
+        res.send('<h1>Library</h1>')
+        res.status(200)
+    })
     
     .listen(3000)
     
+
+    // function notFound(req, res) {
+    //     res.status(404)
+    //     }
