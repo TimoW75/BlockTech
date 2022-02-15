f98e6d0bebe2317fa4f4258c239bbb0140a3fee5
TimoW75 <timo2003werkhoven@gmail.com>
2022-02-15 15:14:33 +0100

    handlebars

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+
 app.use('/static',express.static('static'))
+app.set('view engine', 'hbs');
+app.set('views', 'view');
+
 
 app.get('/', (req, res) => { 
     res.send('<h1>Hello World!</h1>')
@@ -10,7 +14,7 @@ app.get('/', (req, res) => {
     })
 
 app.get('/profiel', (req, res) => { 
-    res.send('<h1>Profiel</h1>')
+    res.render('profiel.hbs')
     res.status(200)
 })
 
