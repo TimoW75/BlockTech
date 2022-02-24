3b1c76676f29100b2af6fc7eeccceee9c16b620c
TimoW75 <timo2003werkhoven@gmail.com>
2022-02-24 22:55:36 +0000

    handlebars templates gemaakt

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+const Handlebars = require("handlebars");
 
 
+
+
+/*********************************************/
+/*Server enzo*/
+/*********************************************/
 app.use('/static',express.static('static'))
 app.set('view engine', 'hbs');
 app.set('views', 'view');
 
 
 app.get('/', (req, res) => { 
-    res.send('<h1>Hello World!</h1>')
+    res.render('profiel.hbs', {
+        profile: 
+        {
+            username: "Nico di Angelo",
+            email: "nicodiangelo@gmail.com",
+            follow: "114 followers   98 following"
+
+        }
+
+
+    })
     res.status(200)
     })
 
-app.get('/profiel', (req, res) => { 
-    res.render('profiel.hbs')
-    res.status(200)
-})
+// app.get('/profiel', (req, res) => { 
+//     res.render('profiel.hbs')
+//     res.status(200)
+// })
 
-app.get('/library', (req, res) => { 
-    res.send('<h1>Library</h1>')
+app.get('/style', (req, res) => { 
+    res.render('filter.hbs')
     res.status(200)
 })
 
@@ -28,3 +44,8 @@ app.get('*', (req, res) => {
 })
 
 app.listen(PORT)   
+
+
+/*********************************************/
+/*Alle dingen enzo*/
+/*********************************************/
