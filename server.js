25b232029e822698ab1b6c916fbcf4afea76bdec
TimoW75 <timo2003werkhoven@gmail.com>
2022-03-13 00:15:14 +0100

    Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 const Handlebars = require("handlebars");
 const sass = require('node-sass');
 const bodyParser = require('body-parser')
