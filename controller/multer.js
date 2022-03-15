4b3d9697e2e5f33bb2c963366db601ea70e3168c
TimoW75 <timo2003werkhoven@gmail.com>
2022-03-15 23:55:30 +0100

    multer

--- controller/multer.js
+++ controller/multer.js
@@ -1,10 +1,10 @@
 const multer = require('multer')
 // set storage
 let storage = multer.diskStorage({ // maak een diskstorage aan
-    destination: function( req, file, cb){
+    destination: ( req, file, cb) => {
         cb(null, './static/upload') // zet het pad voor de storage neer
     },
-    filename: function(req, file, cb){
+    filename: (req, file, cb) =>{
         //returns ext of file, jpg, png, jpep. etc.
         let ext = file.originalname.substr(file.originalname.lastIndexOf('.')) // geef de filename mee
         cb(null, file.fieldname + '-'+Date.now()+ext) // filenaam + de data
